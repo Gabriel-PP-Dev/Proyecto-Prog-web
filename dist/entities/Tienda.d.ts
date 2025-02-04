c64649e92eb1b9e07cdf655c1c541b2880a40419
SofiSteph <solanoestefany55@gmail.com.cu>
2025-02-04 12:57:53 -0500

    burundanga

--- dist/entities/Tienda.d.ts
+++
@@ -1,9 +0,0 @@
-import { Usuario } from "./Usuario";
-import { TiendaProductoPrecio } from "./TiendaProductoPrecio";
-export declare class Tienda {
-    id_tienda: number;
-    nombre: string;
-    direccion: string;
-    usuarios: Usuario[];
-    tiendaProductoPrecios: TiendaProductoPrecio[];
-}
--- dist/entities/Usuario.d.ts
+++
@@ -1,9 +0,0 @@
-import { Tienda } from "./Tienda";
-export declare class Usuario {
-    id_usuario: number;
-    nombre: string;
-    nombre_usuario: string;
-    contrasenna: string;
-    rol: string;
-    tienda: Tienda;
-}
